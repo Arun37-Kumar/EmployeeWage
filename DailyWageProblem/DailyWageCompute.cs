7774960359161d4310973d06c707a8b731c24b3a
Arun37-Kumar <arun007624@gmail.com>
2022-04-06 15:19:58 +0530

    UC5 updated

--- DailyWageProblem/DailyWageCompute.cs
+++ DailyWageProblem/DailyWageCompute.cs
@@ -14,32 +14,40 @@ namespace DailyWageProblem
             const int IS_PART_TIME = 2;
             const int IS_ABSENT = 0;
             const int EMP_RATE_PER_HOURS = 20;
+            const int NUMBER_OF_WORKING_DAYS = 20;
 
             //variables
             int empHrs = 0;
             int empWage = 0;
+            int totalWage = 0;
 
             Random random = new Random();
-            int empCheck = random.Next(0, 3);
 
-            switch (empCheck)
+            for (int days = 0; days < NUMBER_OF_WORKING_DAYS; days++)
             {
-                case IS_PART_TIME:
-                    empHrs = 8;
-                    break;
+                int empCheck = random.Next(0, 3);
 
-                case IS_FULL_TIME:
-                    empHrs = 10;
-                    break;
-                case IS_ABSENT:
-                    empHrs = 0;
-                    break;
-                default:
-                    break;
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 8;
+                        break;
+
+                    case IS_FULL_TIME:
+                        empHrs = 10;
+                        break;
+                    case IS_ABSENT:
+                        empHrs = 0;
+                        break;
+                    default:
+                        break;
+                }
+                totalWage += empWage;
+                empWage = empHrs * EMP_RATE_PER_HOURS;
+
+                Console.WriteLine("Emp Wage : {0}", empWage);
             }
-
-            empWage = empHrs * EMP_RATE_PER_HOURS;
-            Console.WriteLine("Emp Wage : {0}", empWage);
+            Console.WriteLine("Total Employee Wage : " + totalWage);
 
         }
     }
