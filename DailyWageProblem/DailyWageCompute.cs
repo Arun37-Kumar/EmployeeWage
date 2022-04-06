aa00494a2a6827cfab8e73f61637e557d6a1350f
Arun37-Kumar <arun007624@gmail.com>
2022-04-06 14:52:15 +0530

    UC3 updated

--- DailyWageProblem/DailyWageCompute.cs
+++ DailyWageProblem/DailyWageCompute.cs
@@ -10,7 +10,8 @@ namespace DailyWageProblem
     {
         public void EmployeeWageCalculations()
         {
-            int IS_PRESENT = 1;
+            int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 2;
             int IS_ABSENT = 0;
             int EMP_RATE_PER_HOURS = 20;
 
@@ -19,16 +20,18 @@ namespace DailyWageProblem
             int empWage = 0;
 
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
+            int empCheck = random.Next(0, 3);
 
-            if (empCheck == IS_PRESENT)
+            if (empCheck == IS_PART_TIME)
             {
-                Console.WriteLine("Employee is present.");
                 empHrs = 8;
+            }
+            else if (empCheck == IS_FULL_TIME)
+            {
+                empHrs = 10;
             }
             else
             {
-                Console.WriteLine("Employee is absent");
                 empHrs = 0;
             }
             empWage = empHrs * EMP_RATE_PER_HOURS;
